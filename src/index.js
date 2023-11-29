a378348ae2c07b922e3ddaf5f8fbd291e2853556
Daniel Ye <daniel.ye@live.com>
2023-11-28 23:50:49 -0500

    tabs

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import Menu from "./menu";
 import Contact from "./contact";
 
 const content = document.getElementById("content");
+
+const homeTab = document.createElement("a");
+const menuTab = document.createElement("a");
+const contactTab = document.createElement("a");
+
 const home = Home();
 content.append(home);
